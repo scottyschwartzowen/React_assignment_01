cd8b3e1e71d829640457fb24f602873ebe251b4d
scottyschwartz-owen <scottyschwartzowen@icloud.com>
2026-03-01 12:57:59 -0800

    [Feature] Task#3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Greeting from "./components/Greeting.jsx";
-import Counter from './components/Counter.jsx';
-
+import Counter from "./components/Counter.jsx";
+import TasksList from "./components/TasksList.jsx";
 
 function App() {
   return (
     <div>
-      <Greeting username="Alice"/>
-      <Greeting username="Bob" />
+      <Greeting username='Alice' />
+      <Greeting username='Bob' />
       <hr></hr>
       <Counter />
+      <hr></hr>
+      <TasksList />
+      <hr></hr>
     </div>
   );
 }
